9427c08af84020b23da3eb8766f1395552444066
florianbar <florianbar01@gmail.com>
2020-10-28 13:45:49 +0200

    Used getServerSideProps to access query params and added pagination

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+import Link from 'next/link';
 import Error from 'next/error';
 
 import Layout from '../components/Layout';
 import StoryList from '../components/StoryList';
 
-export async function getStaticProps(context) {
+export async function getServerSideProps({ query }) {
     let storiesData = [];
+    const page = Number(query.page) || 1;
 
     try {
-        const response = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+        const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
         storiesData = await response.json();
     } catch (error) {
         console.log(error);
@@ -15,12 +17,13 @@ export async function getStaticProps(context) {
     
     return {
         props: {
-            stories: storiesData
+            stories: storiesData,
+            page
         }
     };
 };
 
-export default function Index({ stories }) {
+const Index = ({ stories, page }) => {
     if (stories.length === 0) {
         return <Error statusCode={503} />;
     }
@@ -28,6 +31,25 @@ export default function Index({ stories }) {
     return (
         <Layout title="Hacker Next" description="A Hacker News Clone made with Next.js">
             <StoryList stories={stories} />
+
+            <footer>
+                <Link href={`/?page=${page + 1}`}>
+                    <a>Page {page + 1}</a>   
+                </Link>
+            </footer>
+
+            <style jsx>{`
+                footer {
+                    padding: 1em;
+                }
+                footer a {
+                    font-weight: bold;
+                    color: #000;
+                    text-decoration: none;
+                }
+            `}</style>
         </Layout>
     );
 };
+
+export default Index;
