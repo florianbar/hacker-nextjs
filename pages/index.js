806bc3193f71fbf00defb073035f7e2ade791acb
florianbar <florianbar01@gmail.com>
2020-10-28 12:29:19 +0200

    Fetch stories data and check for errors

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
-export default function Index() {
+import Error from 'next/error';
+
+export async function getStaticProps(context) {
+    let storiesData = [];
+
+    try {
+        const response = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+        storiesData = await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+    
+    return {
+        props: {
+            stories: storiesData
+        }
+    };
+};
+
+export default function Index({ stories }) {
+    if (stories.length === 0) {
+        return <Error statusCode={503} />;
+    }
+
     return (
-        <div>Hacker Next</div>
+        <div>
+            <h1>Hacker Next</h1>
+            <div>
+                {stories.map(story => (
+                    <h2 key={story.id}>
+                        {story.title}
+                    </h2>
+                ))}
+            </div>
+        </div>
     );
 };
